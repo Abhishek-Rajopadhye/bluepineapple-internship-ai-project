a1350b41f43eaa34b548b3ec067c79ee08f6ef07
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-02-26 10:35:24 +0530

    Changed db name to something more generic.

--- poc-backend/app/config.py
+++ poc-backend/app/config.py
@@ -15,7 +15,7 @@ class Config:
     """
     #Generic Configuration
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///copilot_poc.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///poc.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecretkey")
 
